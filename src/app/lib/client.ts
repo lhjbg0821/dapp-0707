858cdbd566e1cd2d9e846b4f0d097034f639ae03
ollok <lhjbg0821@gmail.com>
2023-07-07 14:17:36 +0900

    metamask sdk 설치

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -1,6 +1,17 @@
 "use client";
 
+import { MetaMaskSDK } from "@metamask/sdk";
+
 import Web3, { Personal } from "web3";
 
-export const web3 = new Web3(window.ethereum);
-export const personal = new Personal(window.ethereum);
+const MMSDK = new MetaMaskSDK({
+  dappMetadata: {
+    name: "MotJa",
+    url: "https://project.project",
+  },
+});
+
+const ethereum = MMSDK.getProvider();
+
+export const web3 = new Web3(ethereum);
+export const personal = new Personal(ethereum);
